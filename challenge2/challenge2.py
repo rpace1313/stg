632431165df7b2c61f35f119a67cec981352bac9
PPace <ppace@cvent.com>
2020-01-28 12:53:19 -0700

    Challenge 2 v1

--- challenge2/challenge2.py
+++ challenge2/challenge2.py
@@ -1,6 +1,7 @@
 import unittest
-import copart_PageMap
+from time import sleep
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 class Challenge2(unittest.TestCase):
 
@@ -17,10 +18,24 @@ class Challenge2(unittest.TestCase):
   #   verify porsche is in the list of cars.
   # Use the hard assertion for this challenge.
   def test_challenge2(self):
+    porcheTrue = False
     self.driver.get("https://copart.com")
-    copart_PageMap.searchBar()
+    search = self.searchBar()
+    search.send_keys('exotics')
+    search.send_keys(Keys.ENTER)
+    sleep(4)
+    makes = self.driver.find_element_by_id('serverSideDataTable')
+    for tr in makes.find_elements_by_tag_name('td'):
+      for span in tr.find_elements_by_tag_name('span'):
+        if span.text == 'PORSCHE':
+          porcheTrue = True
+          break
+        else:
+          continue
+    assert porcheTrue
 
-
+  def searchBar(self):
+    return self.driver.find_element_by_id('input-search')
 
 if __name__ == '__main__':
   unittest.main()
